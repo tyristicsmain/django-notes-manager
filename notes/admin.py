da23c807d93f6cae80a45b6f56578e26d605c1ae
tyristicsmain <svinger20@mail.ru>
2025-11-02 17:48:13 +0700

    model

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'is_important')
+    list_filter = ('category', 'is_important')
--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=500)
+    category = models.IntegerField(default=3)
+    is_important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
